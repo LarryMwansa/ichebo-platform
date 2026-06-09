5456e0447a84cc2e7d4d7d29e6e912fa8e0b49aa
Larry Mwansa <mwansaplarry@gmail.com>
2026-06-09 22:24:43 +0200

    feat: add get_item filter and update template tag loading in Handbook

--- ichebo-platform/core/templatetags/core_tags.py
+++ ichebo-platform/core/templatetags/core_tags.py
@@ -2,6 +2,15 @@ from django import template
 
 register = template.Library()
 
+
+@register.filter
+def get_item(dictionary, key):
+    """Return dictionary[key], or empty string if missing."""
+    if not isinstance(dictionary, dict):
+        return ''
+    return dictionary.get(key, '')
+
+
 @register.filter
 def replace(value, arg):
     """
